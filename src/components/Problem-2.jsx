e4c7d5241ddaef5281d22deeaca0fecfe1670a89
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-05-30 22:24:32 +0600

    show even data not done yet

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -3,31 +3,39 @@ import { useEffect, useState } from "react";
 import { Button, Modal, Spinner } from "react-bootstrap";
 
 const Problem2 = () => {
-    const [data, setData] = useState();
+  const [data, setData] = useState();
+  const [showEvenOnly, setShowEvenOnly] = useState(false);
   const [show, setShow] = useState(false);
   const [loading, setLoading] = useState(true);
   const [url, setUrl] = useState(true);
   const [title, setTitle] = useState(true);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const allUrl = 'https://contact.mediusware.com/api/contacts/?page_size=10';
-  const usUrl = 'https://contact.mediusware.com/api/country-contacts/United%20States/?page_size=5';
-  
-  useEffect(()=>{
+  const allUrl = "https://contact.mediusware.com/api/contacts/?page_size=10";
+  const usUrl =
+    "https://contact.mediusware.com/api/country-contacts/United%20States/?page_size=5";
+
+  useEffect(() => {
     fetch(url ? allUrl : usUrl)
-    .then(res=>res.json())
-    .then(data=>{
+      .then((res) => res.json())
+      .then((data) => {
         setData(data);
         setLoading(false);
-    })
-  },[url])
-  if(loading){
-    return(
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
-    )
+      });
+  }, [url]);
+  const handleCheckboxChange = () => {
+    setShowEvenOnly(!showEvenOnly);
+  };
+    const filteredData = showEvenOnly
+? data.results.filter((item) => item.id % 2 === 0)
+: data;
+setLoading(false);
+  if (loading) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
   }
   return (
     <div className="container">
@@ -35,41 +43,62 @@ const Problem2 = () => {
         <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
 
         <div className="d-flex justify-content-center align-items-center gap-3">
-          <button onClick={()=>{setShow(true), setTitle(true)}} className="btn btn-lg btn-outline-primary" type="button">
+          <button
+            onClick={() => {
+              setShow(true), setTitle(true);
+            }}
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+          >
             All Contacts
           </button>
-          <button onClick={()=>{setShow(true), setTitle(false)}} className="btn btn-lg btn-outline-warning" type="button">
+          <button
+            onClick={() => {
+              setShow(true), setTitle(false);
+            }}
+            className="btn btn-lg btn-outline-warning"
+            type="button"
+          >
             US Contacts
           </button>
         </div>
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>{title ? 'All Contacts':'US Contacts'}</Modal.Title>
+            <Modal.Title>{title ? "All Contacts" : "US Contacts"}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <table>
-                <thead>
-                    <tr>
-                        <th>Phone</th>
-                        <th>Country</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        data.results.map(contact=><tr key={contact.id}>
-                            <td>{contact.phone}</td>
-                            <td>{contact.country.name}</td>
-                        </tr>)
-                    }
-                </tbody>
+              <thead>
+                <tr>
+                  <th>Phone</th>
+                  <th>Country</th>
+                </tr>
+              </thead>
+              <tbody>
+                { filteredData.map((contact) => (
+                  <tr key={contact.id}>
+                    <td>{contact.phone}</td>
+                    <td>{contact.country.name}</td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
           </Modal.Body>
           <Modal.Footer>
+            <label htmlFor="">
+            <input type="checkbox" checked={showEvenOnly} onChange={handleCheckboxChange} />
+            Show even only
+            </label>
+            <Button
+              onClick={() => {
+                setUrl(!url), setShow(true), setTitle(!title);
+              }}
+              variant="primary"
+            >
+              {title ? "US Contacts" : "All Contacts"}
+            </Button>
             <Button variant="secondary" onClick={handleClose}>
               Close
-            </Button>
-            <Button onClick={()=>{setUrl(!url), setShow(true), setTitle(!title)}} variant="primary">
-              {title ? 'US Contacts' : 'All Contacts'}
             </Button>
           </Modal.Footer>
         </Modal>
